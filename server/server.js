c42027a7072e5b452104f8515fc3938a7fd1fad9
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-09-26 07:07:27 +0100

    removing comments

--- server/server.js
+++ server/server.js
@@ -29,32 +29,6 @@ app.get("/fetchRepoData", async (req, res) => {
   }
 });
 
-/*
-app.get("/", async (req, res) => {
-  // Your code to fetch and display graduate data goes here
-  // Inside the app.get('/', ...) route handler
-  const username = "ali-nasir-ali"; // Replace with the graduate's GitHub username
-  const repository = "ali-nasir-ali"; // Replace with the repository name
-  // https://github.com/ali-nasir-ali/ali-nasir-ali/blob/main/README.md
-
-  
-  const response = await axios.get(
-    `https://api.github.com/repos/${username}/${repository}`
-    // {
-    //   headers: {
-    //     Authorization: "Bearer YOUR_GITHUB_TOKEN",
-    //   },
-    // }
-  );
-
-  const readmeContent = Buffer.from(response.data.content, "base64").toString();
-
-  // You can parse the README content here and send it as a JSON response
-  // For now, let's send the raw content as is
-  res.send(readmeContent);
-});
-*/
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
