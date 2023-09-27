e9e2641e3804fc5cbe2c61844822a2a8fa82e5e1
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-09-27 20:06:37 +0100

    get data from the API

---
+++ server/DBConfig.js
@@ -0,0 +1,12 @@
+// const { Pool } = require("pg");
+
+// const videosPool = new Pool({
+//   connectionString:
+//     "postgres://testuser:t2rUufCbNTZy45viv2A7faB5Fc2NODAG@dpg-ck8rp4nsasqs738mn1eg-a.oregon-postgres.render.com/muath_alawadhi",
+
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// module.exports = videosPool;
--- server/server.js
+++ server/server.js
@@ -1,40 +1,74 @@
-//Issue number 4
-//Given a github username I want to retrieve information about this person's
-//repos and store it in the database:
-//Access to github API
-
-const { Octokit } = require("@octokit/core");
+//---------- Read READMI.md if the code doesn't work on your local laptop  ---------------
 const express = require("express");
 const app = express();
-const port = 3000;
-// const dotenv = require("dotenv").config();
-const octokit = new Octokit({
-  auth: `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`, //will use env once testing is done
-});
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser"); //is a middleware for Express.js
+const port = process.env.PORT || 3002;
 
+const fetch = (
+  ...args ///////fetching (node.js library)
+) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+//------------------------GitHub OAuth (1)Apps information ----------------------
+//// the ClintID and ClintSecret info on the env file
+const ClintID = process.env.GITHUB_CLIENT_ID;
+const ClintSecret = process.env.GITHUB_CLIENT_SECRET;
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+//----------------testing (2)the gte--------------------------//
 app.get("/", (req, res) => {
   console.log("welcome to my server");
 });
+//--------------------------------------------------------//
 
-//fetch Repos data
-app.get("/fetchRepoData", async (req, res) => {
-  try {
-    // Use Octokit to make an API request to GitHub
-    const response = await octokit.request("GET /repos/{owner}/{repo}", {
-      owner: "rahmab1",
-      repo: "fp-repo",
+//-------------- code being passed (3) from the frontend----//
+app.get("/getAccessToken", async (req, res) => {
+  console.log(req.query.code); // testing or console the outputwe will be a code from the request
+  //and we will use it to access to get  the token
+
+  const params = `?client_id=${ClintID}&client_secret=${ClintSecret}&code=${req.query.code}`;
+
+  await fetch("https://github.com/login/oauth/access_token" + params, {
+    ///// posting the token
+    method: "post",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("result", data); ///// testing the output
+      res.json(data);
     });
-
-    const repositories = response.data;
-
-    // Send the data as a JSON response
-    res.json({ repositories });
-  } catch (error) {
-    console.error("Error fetching data from GitHub:", error.message);
-    res.status(500).json({ error: "Failed to fetch data from GitHub" });
-  }
 });
+//--------------------------------------------------------//
+
+////-------------------Get user(4) data---------------------//
+app.get("/getUserData", async (req, res) => {
+  await fetch("https://api.github.com/user", {
+    method: "GET",
+    headers: {
+      Authorization: await req.get("Authorization"),
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
+});
+//--------------------------------------------------------//
+
+//--------------------Port listen(5) HERE-------------------//
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+//------------------------------------------------------//
