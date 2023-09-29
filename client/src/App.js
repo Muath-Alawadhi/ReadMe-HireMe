3b903e5edd9996bdc9c45a7aac01075b0f086db1
Najah Farah <najahfarah27@gmail.com>
2023-09-29 19:34:55 +0100

    api + database interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import "./App.css";
+import React from 'react';
+import './App.css';
+import GitHubUserInfo from './GitHubUserInfo'; // Make sure to import it correctly
 
 function App() {
   return (
     <div className="App">
-      <h1> Our Project</h1>
+      <h1>Our Project</h1>
+      <GitHubUserInfo />
     </div>
   );
 }
 
 export default App;
+
