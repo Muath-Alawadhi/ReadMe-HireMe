14b497398d997152093916e29b105514a26e1f63
siveromar <109865820+siveromar@users.noreply.github.com>
2023-09-29 13:09:14 +0100

    profile -page

---
+++ client/src/SignUpComponents/SUGitHubContainer.js
@@ -0,0 +1,18 @@
+import "./SUStyle.css";
+import React, { useState } from 'react';
+
+function SUGitHubContainer() {
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const handleClick = () => {
+    setButtonClicked(true);
+  };
+    return (
+      <div className="SUGitHubBlock">
+        <button onClick={handleClick}>Sign Up With GitHub</button>
+      {buttonClicked && console.log("GitHubSignUp")}
+      </div>
+    );
+  }
+  
+  export default SUGitHubContainer;
