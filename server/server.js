6734eaab8a56496b41ec0cb46621cae8e8f4c3f2
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-09-30 11:47:53 +0100

    adding repo data

--- server/server.js
+++ server/server.js
@@ -73,6 +73,30 @@ app.get("/getUserData", async (req, res) => {
 });
 //--------------------------------------------------------//
 
+//---------------------Add Repo Data------------------------//
+//This is for test purposes , adding username and repo name manually
+//
+
+app.get("/fetchRepoData", async (req, res) => {
+  try {
+    // Use Octokit to make an API request to GitHub
+    const response = await octokit.request("GET /repos/{owner}/{repo}", {
+      owner: "rahmab1",
+      repo: "fp-repo",
+    });
+
+    const repositories = response.data;
+
+    // Send the data as a JSON response
+    res.json({ repositories });
+  } catch (error) {
+    console.error("Error fetching data from GitHub:", error.message);
+    res.status(500).json({ error: "Failed to fetch data from GitHub" });
+  }
+});
+
+//--------------------------------------------------------//
+
 //--------------------Port listen(5) HERE-------------------//
 
 app.listen(port, () => {
