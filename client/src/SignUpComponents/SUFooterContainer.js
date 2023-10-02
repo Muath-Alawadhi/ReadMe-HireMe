7a341a1eaa946efc3470a59d38cc6476a2bc7563
siveromar <109865820+siveromar@users.noreply.github.com>
2023-10-02 21:35:41 +0100

    2nd-test

--- client/src/SignUpComponents/SUFooterContainer.js
+++ client/src/SignUpComponents/SUFooterContainer.js
@@ -3,7 +3,7 @@ import "./SUStyle.css";
 function SUFooterContainer() {
     return (
       <div className="SUFooterBlock">
-        <p> the change</p>
+        <p> the  new change</p>
       </div>
     );
   }
