61ed28d7db575364be6eb1b1df47422f9c656b11
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-10-03 06:17:29 +0100

    testing regex

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import GitHubUserInfo from "./GitHubUserInfo";
 
 function App() {
   return (
     <div className="App">
       <h1> Our Project</h1>
+      <GitHubUserInfo />
     </div>
   );
 }
