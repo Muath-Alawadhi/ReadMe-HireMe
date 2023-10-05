9468f0b3874c476a03ea9c3f4951256fbfd3ed6d
Najah Farah <najahfarah27@gmail.com>
2023-10-05 19:38:21 +0100

    Dbconfig

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 // const { Pool } = require("pg");
 
-const pool = require('./dbConfig');
+const pool = require('./DbConfig');
 
 const port = 6000;
 
