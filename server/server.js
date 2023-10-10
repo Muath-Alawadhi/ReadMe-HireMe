afef01f3e1e29ea25ece0a5c199a06081151b9f1
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-10-10 21:31:08 +0100

    updating the code with new axios //library

--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
 //To Do:
-//add env for port and token
-//find solution for regular token expiration
 //we still need skills from repos path
-//
 //sometimes when trying to test code , we are having issues with DB connection
 //so, we just need comment out all DB code in order to test other parts, then uncomment them
 //or we can wait for some time till the DB connection is back to normal
@@ -47,14 +44,12 @@ app.post("/access-code", async (req, res) => {
 
   const { access_token } = response.data;
   githubAccessToken = access_token;
-  console.log(githubAccessToken); // printing the access token
+  console.log(githubAccessToken); // printing the access token for checking
 
   // After getting the token, immediately fetch and insert data into the database
   await fetchAndInsertData();
   res.json({ success: true });
 });
-
-//declate object here to store all data from api response
 
 //------------------ get / ---------------------
 app.get("/", (req, res) => {
@@ -62,122 +57,127 @@ app.get("/", (req, res) => {
 });
 
 //----------------- fetch Grad data ------------------
+async function fetchAndInsertData() {
+  app.get("/fetchGradData", async (req, res) => {
+    const client = await pool.connect();
+    try {
+      //--(1)--giving username fetch --> name , repos number , profile_pic_url ---
+      const userDataResponse = await axios.get("https://api.github.com/user", {
+        headers: {
+          Authorization: `token ${githubAccessToken}`,
+        },
+      });
+      console.log(userDataResponse);
 
-app.get("/fetchGradData", async (req, res) => {
-  const client = await pool.connect();
-  try {
-    //--(1)--giving username fetch --> name , repos number , profile_pic_url ---
+      const userData = userDataResponse.data;
+      const githubUserName = userData.login || "Not available";
+      const name = userData.name || "Name Not available";
+      const reposNumber = userData.public_repos || "Not available";
+      const profilePicLink = userData.avatar_url || "Not available";
+      // console.log(githubUserName, name, reposNumber, profilePicLink);
 
-    const userDataResponse = await axios.get("https://api.github.com/user", {
-      headers: {
-        Authorization: `token ${githubAccessToken}`,
-      },
-    });
-    console.log(userDataResponse);
+      // ---------------------repo.languages--------------------------
+      const reposResponse = await axios.get(userData.repos_url, {
+        // Using the repos_url to fetch repositories first ^ ^
+        headers: {
+          Authorization: `token ${githubAccessToken}`,
+        },
+      });
+      // Extract language data from repositories...o-o
+      const repos = reposResponse.data;
+      console.log(repos);
 
-    const userData = response.data;
-    // console.log(userData);
+      const uniqueLanguages = new Set();
 
-    const githubUserName = userData.login || "Not available";
-    const name = userData.name || "Name Not available";
-    const reposNumber = userData.public_repos || "Not available";
-    const profilePicLink = userData.avatar_url || "Not available";
-    // console.log(githubUserName, name, reposNumber, profilePicLink);
-
-    // ---------------------repo.languages--------------------------
-    const reposUrl = userData.repos_url;
-    // Using the repos_url to fetch repositories first ^ ^
-    const reposResponse = await octokit.request("GET " + reposUrl);
-
-    // Extract language data from repositories...o-o
-    const repos = reposResponse.data;
-
-    const uniqueLanguages = new Set();
-
-    repos.forEach((repo) => {
-      const language = repo.language;
-
-      if (language && language !== "null") {
-        uniqueLanguages.add(language);
+      // Iterate through user's repositories to extract languages
+      for (const repo of repos) {
+        const language = repo.language;
+        if (language && language !== "null") {
+          uniqueLanguages.add(language);
+        }
       }
-    });
 
-    const allLanguages = [...uniqueLanguages];
+      const allLanguages = [...uniqueLanguages];
+      //-------------------end of repo.languages ----------------------
 
-    //-------------------end of repo.languages ----------------------
+      //  ------------------- fetch readme file  ----------------------
+      const readmeDataResponse = await axios.get(
+        `https://api.github.com/repos/${githubUserName}/${githubUserName}/readme`,
 
-    //------------------- fetch readme file  ----------------------
-    const readmeDataResponse = await octokit.request(
-      "Get /repos/{owner}/{repo}/readme",
-      { owner: "rahmab1", repo: "rahmab1" }
-    );
-    // The README content will be in base64-encoded,so we need to decode it
-    const readmeContent = Buffer.from(
-      readmeDataResponse.data.content,
-      "base64"
-    ).toString("utf-8");
-    // console.log(readmeContent);
+        {
+          headers: {
+            Authorization: `token ${githubAccessToken}`,
+          },
+        }
+      );
 
-    // cvRegex & linkedinRegex to match CV and LinkedIn links
-    const cvRegex =
-      /(?:cv|resume|portfolio)\s*:\s*?(https?:\/\/(?:www\.)?(?:[a-zA-Z0-9-]+\.)+(?:[a-zA-Z]{2,})(?:\/[^\s]*)?)/i;
-    const linkedinRegex = /(https?:\/\/www\.linkedin\.com\/\S+)/i;
+      // The README content will be in base64-encoded,so we need to decode it
+      const readmeContent = Buffer.from(
+        readmeDataResponse.data.content,
+        "base64"
+      ).toString("utf-8");
+      // console.log(readmeContent);
 
-    // Search for CV and LinkedIn links in the README content
-    const cvMatch = readmeContent.match(cvRegex);
-    const linkedinMatch = readmeContent.match(linkedinRegex);
+      // cvRegex & linkedinRegex to match CV and LinkedIn links
+      const cvRegex =
+        /(?:cv|resume|portfolio)\s*:\s*?(https?:\/\/(?:www\.)?(?:[a-zA-Z0-9-]+\.)+(?:[a-zA-Z]{2,})(?:\/[^\s]*)?)/i;
+      const linkedinRegex = /(https?:\/\/www\.linkedin\.com\/\S+)/i;
 
-    const cvLink = cvMatch ? cvMatch[0] : "CV link not found";
-    const linkedin = linkedinMatch
-      ? linkedinMatch[0]
-      : "LinkedIn link not found";
+      // Search for CV and LinkedIn links in the README content
+      const cvMatch = readmeContent.match(cvRegex);
+      const linkedinMatch = readmeContent.match(linkedinRegex);
 
-    //still need to add cvLink and linkedin to DB in separate branch
+      const cvLink = cvMatch ? cvMatch[0] : "CV link not found";
+      const linkedin = linkedinMatch
+        ? linkedinMatch[0]
+        : "LinkedIn link not found";
 
-    //------------------- end of readme file  ----------------------
+      // still need to add cvLink and linkedin to DB in separate branch
 
-    await client.query("BEGIN"); // starting client
-    //  putting Grad  data into the test_graduate  table and
-    const insertQuery = `
-      INSERT INTO Test_Graduate(userName, name, repos_number, profile_pic, skills)
-      VALUES ($1, $2, $3, $4, $5)
-      RETURNING id
-    `;
+      //------------------- end of readme file  ----------------------
 
-    // putting the  values in an array in the table  and storing the result.
-    const values = [
-      githubUserName,
-      name,
-      reposNumber,
-      profilePicLink,
-      allLanguages,
-    ];
-    const result = await client.query(insertQuery, values);
+      // await client.query("BEGIN"); // starting client
+      // //  putting Grad  data into the test_graduate  table and
+      // const insertQuery = `
+      //   INSERT INTO Test_Graduate(userName, name, repos_number, profile_pic, skills)
+      //   VALUES ($1, $2, $3, $4, $5)
+      //   RETURNING id
+      // `;
 
-    // // committing client
-    await client.query("COMMIT");
+      // putting the  values in an array in the table  and storing the result.
+      // const values = [
+      //   githubUserName,
+      //   name,
+      //   reposNumber,
+      //   profilePicLink,
+      //   allLanguages,
+      // ];
+      // const result = await client.query(insertQuery, values);
 
-    client.release();
+      // // Commit the client
+      // await client.query("COMMIT");
 
-    //Send the data as a JSON response
-    res.json({
-      userName: githubUserName,
-      name: name,
-      repos_number: reposNumber,
-      profile_pic: profilePicLink,
-      skills: allLanguages,
-      cv: cvLink,
-      linkedIn: linkedin,
-    });
-  } catch (error) {
-    console.error("Error fetching data from GitHub:", error.message);
-    res.status(500).json({ error: "Failed to fetch data from GitHub" });
-    await client.query("ROLLBACK");
-    throw new Error(
-      "Failed to insert data into the database. Please try again later."
-    );
-  }
-});
+      // client.release();
+
+      //Send the data as a JSON response
+      res.json({
+        userName: githubUserName,
+        name: name,
+        repos_number: reposNumber,
+        profile_pic: profilePicLink,
+        skills: allLanguages,
+        cv: cvLink,
+        linkedIn: linkedin,
+      });
+    } catch (error) {
+      console.error("Error fetching and inserting data:", error.message);
+      // await client.query("ROLLBACK");
+      throw new Error(
+        "Failed to insert data into the database. Please try again later."
+      );
+    }
+  });
+}
 
 //------------------ Endpoint for FrontEnd --------------
 
