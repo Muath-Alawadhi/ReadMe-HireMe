83abbb83f98b04f06a511b3168f177b679d7080d
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-10-10 18:33:02 +0100

    signup authentication frontend => fixed

--- client/src/components/signup/Signup.jsx
+++ client/src/components/signup/Signup.jsx
@@ -1,32 +1,50 @@
 import { Link } from "react-router-dom";
 import "./Signup.css";
+import { useEffect } from "react";
 
 function Signup() {
+  //signup authentication
   const handleLoginClick = () => {
-    const ClientID = "4b7ee8342a11859ac626";
-    const redirectUri = "http://localhost:3000/github/callback";
-    const oauthURL =
-      `https://github.com/login/oauth/authorize` +
-      `?client_id=${ClientID}` +
-      `&redirect_uri=${encodeURIComponent(redirectUri)}`;
-
-    window.location.assign(oauthURL);
+    const clientId = "4b7ee8342a11859ac626";
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}`;
   };
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const code = urlParams.get("code");
+    console.log(code);
+
+    if (code) {
+      // Send the code to backend to exchange it for an access token
+      fetch("http://localhost:8000/access-code", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Access Token:", data.access_token); // testing the Access result
+        })
+        .catch((error) => {
+          console.error("Error exchanging code for access token:", error);
+        });
+    }
+  }, []);
 
   return (
     <div className="SignupBlock">
-       <div className="Hire-me">
-        <span className="HireColor">Hire  </span>
+      <div className="Hire-me">
+        <span className="HireColor">Hire </span>
         <span className="MeColor">Me</span>
       </div>
       <p className="click-button-text">Click The Button To Sign Up:</p>
-      <div><button className="button" onClick={handleLoginClick}> <i className="fa-brands fa-github"></i> Sign Up With GitHub</button>
-      <link
-        rel="stylesheet"
-        href="https://fontawesome.com/icons/github?f=brands&s=solid"
-        integrity="sha384-oBqDVmMzT8r8aFNj3ePMiM5l7zrKwR3i0LY0XJiDpjjD5z5qn5BUNB6e8JPW+qIr"
-        crossOrigin="anonymous"
-      />
+      <div>
+        <button className="button" onClick={handleLoginClick}>
+          <i className="fa-brands fa-github"></i> Sign Up With GitHub
+        </button>
       </div>
       <p className="Already-Signed-Up">
         Already Signed Up? <Link to="/graduates">Go To Graduates</Link>
