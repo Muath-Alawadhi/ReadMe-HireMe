a52edc19b391411b0409fa064d2bf0c4d8c91da3
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-10-20 22:44:21 +0100

    fixing small letters

--- client/src/components/Graduates/SearchBar.jsx
+++ client/src/components/Graduates/SearchBar.jsx
@@ -1,23 +1,43 @@
 import "./SearchBar.css";
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { InputGroup, FormControl , Button  } from 'react-bootstrap';
-function SearchBar({ onSearchResults}) {
-  const [searchQuery, setSearchQuery] = useState('');
-  const handleSearch = async () => {
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { InputGroup, FormControl, Button } from "react-bootstrap";
+
+function SearchBar({ onSearchResults, allGraduates }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = async (query) => {
     try {
-     const response = await fetch(`http://localhost:8000/api/search?query=${searchQuery}`);
+      const cleanedQuery = query;
+
+      const response = await fetch(
+        `http://localhost:8000/api/search?query=${cleanedQuery}`
+      );
       const responseData = await response.json();
       const filteredData = responseData.graduates;
+      console.log("d", filteredData);
+
       if (response.ok) {
         onSearchResults(filteredData);
       } else {
-        console.error("Failed to fetch filteredData:",filteredData);
+        console.error("Failed to fetch filteredData:", filteredData);
       }
     } catch (err) {
       console.error("An error occurred:", err);
     }
   };
+
+  const handleInputChange = (e) => {
+    const newValue = e.target.value;
+    setSearchQuery(newValue);
+
+    if (newValue) {
+      handleSearch(newValue);
+    } else {
+      onSearchResults(allGraduates);
+    }
+  };
+
   return (
     <div className="container mt-4">
       <h1>Search a graduate</h1>
@@ -26,21 +46,15 @@ function SearchBar({ onSearchResults}) {
           placeholder="Search by name or skill..."
           aria-label="Search"
           aria-describedby="basic-addon2"
-          onChange={(e) => {setSearchQuery(e.target.value)
-          }
-        }
+          onChange={handleInputChange}
           value={searchQuery}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              handleSearch();
-            }
-          }}
         />
-          <Button variant="danger" onClick={handleSearch}>
-            <i className="fas fa-search"></i>
-          </Button>
+        <Button variant="danger" onClick={() => handleSearch(searchQuery)}>
+          <i className="fas fa-search"></i>
+        </Button>
       </InputGroup>
     </div>
   );
 }
+
 export default SearchBar;
