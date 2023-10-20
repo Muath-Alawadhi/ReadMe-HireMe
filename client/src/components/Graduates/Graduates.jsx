7eeafbf919422698de311b039520dd214354aefa
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-10-20 23:20:03 +0100

    fixed displaying search

--- client/src/components/Graduates/Graduates.jsx
+++ client/src/components/Graduates/Graduates.jsx
@@ -1,6 +1,4 @@
-// For installing the last version of react-router-dom
-// npm install react-router-dom@latest
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Graduates.css";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -8,67 +6,54 @@ import SearchBar from "./SearchBar";
 import { Link } from "react-router-dom";
 import GraduatesProfile from '../GraduatesProfile/GraduatesProfile';
 
-
-
 function GradCard({ grad, onViewMore }) {
   return (
     <div>
-       <div>
-       <Card style={{ width: '17rem' }} key={grad.id}>
-             <Card.Img variant="top" src={grad.profile_pic_link} alt={grad.name} className="cards-img" />
-             <Card.Body>
-              <Card.Title>{grad.name}</Card.Title>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>Username: {grad.github_username}</ListGroup.Item>
-              <ListGroup.Item>Repo: {grad.repos_number}</ListGroup.Item>
-              <ListGroup.Item>Skills: {grad?.skills || grad.languages} </ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-              <Link onClick={() => onViewMore(grad)}>View More</Link>
-            </Card.Body>
-          </Card>
-    </div>
+      <div>
+        <Card style={{ width: '17rem' }} key={grad.id}>
+          <Card.Img variant="top" src={grad.profile_pic_link} alt={grad.name} className="cards-img" />
+          <Card.Body>
+            <Card.Title>{grad.name}</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Username: {grad.github_username}</ListGroup.Item>
+            <ListGroup.Item>Repo: {grad.repos_number}</ListGroup.Item>
+            <ListGroup.Item>Skills: {grad?.skills || grad.languages}</ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Link onClick={() => onViewMore(grad)}>View More</Link>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
 
-
 function Graduates() {
-
-    const [graduates, setGraduates] = useState([]);
-    const [filteredGraduates, setFilteredGraduates] = useState(null);
-
+  const [graduates, setGraduates] = useState([]);
+  const [filteredGraduates, setFilteredGraduates] = useState(null);
   const [selectedGrad, setSelectedGrad] = useState(null);
-  const [isLoading, setIsLoading] = useState(true); // Add a loading state
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleSearchResults = (filteredData) => {
     setFilteredGraduates(filteredData);
   };
 
-
-
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:8000/api/fetchGradData` );
-         const responseData = await response.json();
-        const data = responseData.graduates; // Access the 'graduates' key as the response from api is--> res.json({ graduates: grads });
+        const response = await fetch(`http://localhost:8000/api/fetchGradData`);
+        const responseData = await response.json();
+        const data = responseData.graduates;
         console.log("Data from API:", data);
         setGraduates(data);
-        setIsLoading(false); 
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
     fetchData();
-  },[]);
-
-useEffect(() => {
-  console.log(graduates);
-}, [graduates]);
-
+  }, []);
 
   const handleViewMore = (grad) => {
     setSelectedGrad(grad);
@@ -84,17 +69,20 @@ useEffect(() => {
         <GraduatesProfile grad={selectedGrad} onGoBack={handleGoBack} />
       ) : (
         <div className="grad-cards">
-           <SearchBar onSearchResults={handleSearchResults}  />
-           <div className="CardsContainerBlock">
-            {isLoading ? ( // Check loading state
+          <SearchBar onSearchResults={handleSearchResults} />
+          <div className="CardsContainerBlock">
+            {isLoading ? (
               <p>Loading data...</p>
             ) : (
-              (filteredGraduates || graduates).map((grad) => (
-                <GradCard key={grad.id} grad={grad} onViewMore={handleViewMore} />
+              ((filteredGraduates || graduates).length === 0 ? (
+                <p className='results-display'>No results to display.</p>
+              ) : (
+                (filteredGraduates || graduates).map((grad) => (
+                  <GradCard key={grad.id} grad={grad} onViewMore={handleViewMore} />
+                ))
               ))
             )}
           </div>
-
         </div>
       )}
     </div>
@@ -102,4 +90,3 @@ useEffect(() => {
 }
 
 export default Graduates;
-
