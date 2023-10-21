c88c8676b10c3e8125f937d0765a8f2104dd516a
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-10-21 20:49:31 +0100

    fixed xss

--- client/src/components/Graduates/Graduates.jsx
+++ client/src/components/Graduates/Graduates.jsx
@@ -1,45 +1,48 @@
 // For installing the last version of react-router-dom
 // npm install react-router-dom@latest
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./Graduates.css";
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 import SearchBar from "./SearchBar";
-import GraduatesProfile from '../GraduatesProfile/GraduatesProfile';
-import {
- 
-  MDBBtn,
- 
-} from "mdb-react-ui-kit";
-
+import GraduatesProfile from "../GraduatesProfile/GraduatesProfile";
+import { MDBBtn } from "mdb-react-ui-kit";
 
 function GradCard({ grad, onViewMore }) {
   return (
-
-       <Card style={{ width: '17rem' }} key={grad.id}>
-             <Card.Img variant="top" src={grad.profile_pic_link} alt={grad.name} className="cards-img" />
-             <Card.Body>
-              <Card.Title>{grad.name}</Card.Title>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>Username: {grad.github_username}</ListGroup.Item>
-              <ListGroup.Item>Repo: {grad.repos_number}</ListGroup.Item>
-              <ListGroup.Item>Skills: {grad?.skills || grad.languages} </ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
-             <MDBBtn onClick={() => onViewMore(grad)} outline className="ms-1  backButton">
-                 View More
-              </MDBBtn>
-            </Card.Body>
-          </Card>
+    <Card style={{ width: "17rem" }} key={grad.id}>
+      <Card.Img
+        variant="top"
+        src={grad.profile_pic_link}
+        alt={grad.name}
+        className="cards-img"
+      />
+      <Card.Body>
+        <Card.Title>{grad.name}</Card.Title>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Username: {grad.github_username}</ListGroup.Item>
+        <ListGroup.Item>Repo: {grad.repos_number}</ListGroup.Item>
+        <ListGroup.Item>
+          Skills: {grad?.skills || grad.languages}
+        </ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <MDBBtn
+          onClick={() => onViewMore(grad)}
+          outline
+          className="View-More"
+        >
+          View More
+        </MDBBtn>
+      </Card.Body>
+    </Card>
   );
 }
 
-
 function Graduates() {
-
-    const [graduates, setGraduates] = useState([]);
-    const [filteredGraduates, setFilteredGraduates] = useState(null);
+  const [graduates, setGraduates] = useState([]);
+  const [filteredGraduates, setFilteredGraduates] = useState(null);
 
   const [selectedGrad, setSelectedGrad] = useState(null);
   const [isLoading, setIsLoading] = useState(true); // Add a loading state
@@ -48,29 +51,25 @@ function Graduates() {
     setFilteredGraduates(filteredData);
   };
 
-
-
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:8000/api/fetchGradData` );
-         const responseData = await response.json();
+        const response = await fetch(`http://localhost:8000/api/fetchGradData`);
+        const responseData = await response.json();
         const data = responseData.graduates; // Access the 'graduates' key as the response from api is--> res.json({ graduates: grads });
         console.log("Data from API:", data);
         setGraduates(data);
-        setIsLoading(false); 
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
     fetchData();
-  },[]);
+  }, []);
 
-useEffect(() => {
-  console.log(graduates);
-}, [graduates]);
-
+  useEffect(() => {
+    console.log(graduates);
+  }, [graduates]);
 
   const handleViewMore = (grad) => {
     setSelectedGrad(grad);
@@ -86,17 +85,20 @@ useEffect(() => {
         <GraduatesProfile grad={selectedGrad} onGoBack={handleGoBack} />
       ) : (
         <div className="grad-cards">
-           <SearchBar onSearchResults={handleSearchResults}  />
-           <div className="CardsContainerBlock">
-            {isLoading ? ( 
+          <SearchBar onSearchResults={handleSearchResults} />
+          <div className="CardsContainerBlock">
+            {isLoading ? (
               <p>Loading data...</p>
             ) : (
               (filteredGraduates || graduates).map((grad) => (
-                <GradCard key={grad.id} grad={grad} onViewMore={handleViewMore} />
+                <GradCard
+                  key={grad.id}
+                  grad={grad}
+                  onViewMore={handleViewMore}
+                />
               ))
             )}
           </div>
-
         </div>
       )}
     </div>
@@ -104,4 +106,3 @@ useEffect(() => {
 }
 
 export default Graduates;
-
