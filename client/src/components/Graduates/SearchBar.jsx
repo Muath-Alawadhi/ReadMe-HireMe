17b97958ab1a2837b4dd617aaf2d5241bd623485
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-10-23 21:54:49 +0100

    api

--- client/src/components/Graduates/SearchBar.jsx
+++ client/src/components/Graduates/SearchBar.jsx
@@ -1,44 +1,46 @@
 import "./SearchBar.css";
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { InputGroup, FormControl , Button  } from 'react-bootstrap';
-function SearchBar({ onSearchResults}) {
-  const [searchQuery, setSearchQuery] = useState('');
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { InputGroup, FormControl, Button } from "react-bootstrap";
+function SearchBar({ onSearchResults }) {
+  const [searchQuery, setSearchQuery] = useState("");
   const handleSearch = async () => {
     try {
-     const response = await fetch(`http://localhost:8000/api/search?query=${searchQuery}`);
+      const response = await fetch(
+        `https://readme-hireme.onrender.com/api/search?query=${searchQuery}`
+      );
       const responseData = await response.json();
       const filteredData = responseData.graduates;
       if (response.ok) {
         onSearchResults(filteredData);
       } else {
-        console.error("Failed to fetch filteredData:",filteredData);
+        console.error("Failed to fetch filteredData:", filteredData);
       }
     } catch (err) {
       console.error("An error occurred:", err);
     }
   };
   return (
-    <div className="SearchBlock" >
+    <div className="SearchBlock">
       <h2 className="search-title">Search a graduate</h2>
       <InputGroup className="mb-3">
         <FormControl
           placeholder="Search a graduate by name or skill..."
           aria-label="Search"
           aria-describedby="basic-addon2"
-          onChange={(e) => {setSearchQuery(e.target.value)
-          }
-        }
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+          }}
           value={searchQuery}
           onKeyPress={(e) => {
-            if (e.key === 'Enter') {
+            if (e.key === "Enter") {
               handleSearch();
             }
           }}
         />
-          <Button variant="danger" onClick={handleSearch}>
-            <i className="fas fa-search"></i>
-          </Button>
+        <Button variant="danger" onClick={handleSearch}>
+          <i className="fas fa-search"></i>
+        </Button>
       </InputGroup>
     </div>
   );
